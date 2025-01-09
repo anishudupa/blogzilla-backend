48d7179e090a6fb7565687d10c891ce70a20c04b
anishudupa <anishgithub1@gmail.com>
2025-01-09 21:00:22 +0530

    Changes

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,5 +1,5 @@
 import connectDB from "../src/config/db";
-import app from "../src/app";
+import app from "../src/server";
 import dotenv from "dotenv";
 
 dotenv.config();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,9 @@
 import app from "./app";
-import connectDB from "./config/db";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-(async function () {
-	try {
-		await connectDB();
-		const PORT = process.env.PORT;
-		app.listen(PORT ?? 5000, () =>
-			console.log("SERVER IS RUNNING ON PORT", PORT)
-		);
-	} catch (error) {
-		console.log(error);
-	}
-})();
+const PORT = process.env.PORT;
+app.listen(PORT ?? 5000, () => console.log("SERVER IS RUNNING ON PORT", PORT));
+
+export default app;
